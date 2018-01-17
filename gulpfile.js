3ed4f44c760b350e5a40111dfe156d876397e73f
GavinChenz <532686661@qq.com>
2018-01-17 21:21:59 +0800

    今天老死机，很郁闷，先写到这吧，nav还没完成

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,47 @@ var gulp=require('gulp'),
 sass= require('gulp-sass'),
 connect=require('gulp-connect'),
 concat= require('gulp-concat'),
-uglify=require("gulp-uglofy"),
+uglify=require("gulp-uglify"),
 rename=require("gulp-rename"),
 ninfyCSS=require("gulp-minify-css"),
 imagemin=require("gulp-imagemin");
 
-//定义一个testLess任务（自定义任务名称）
-gulp.task('testLess',function(){
-	gulp.src('src/sass/index.scss')
+gulp.task("sass",function(){
+	gulp.src("sass/*.scss")
 	.pipe(sass())
-	.pipe(gulp.dest('src/css'));
+	.pipe(gulp.dest("D:\\陈卓\\Item_cz_2018_01_16\\css"))
+});
+
+gulp.task('copy-html',function(){
+	gulp.src('*.html')
+	.pipe(gulp.dest('D:\\phpStudy\\WWW\\CakeItem'));
+});
+
+gulp.task('copyimgfile',function(){
+	gulp.src('img/**/*')
+	.pipe(gulp.dest('D:\\phpStudy\\WWW\\CakeItem\\img'));
+});
+
+gulp.task('copycssfile',function(){
+	gulp.src('css/**/*')
+	.pipe(gulp.dest('D:\\phpStudy\\WWW\\CakeItem\\css'));
+});
+
+
+gulp.task('copyjsgfile',function(){
+	gulp.src('js/**/*')
+	.pipe(gulp.dest('D:\\phpStudy\\WWW\\CakeItem\\js'));
+});
+
+gulp.task('copyvideogfile',function(){
+	gulp.src('video/**/*')
+	.pipe(gulp.dest('D:\\phpStudy\\WWW\\CakeItem\\video'));
+});
+
+// gulp.task("build",["sass"],function(){
+// 	console.log("ok");
+// });
+
+gulp.task("watch",function(){
+	gulp.watch("sass/*.scss",["sass"]);
 });
